444502da21ba04255d95ece6bb3a40789f446e64
Senthil Rajendran <rajsenthil@yahoo.com>
2022-04-14 19:24:00 -0700

    whitespcae fixes

--- algorithms/fibonocci.go
+++ algorithms/fibonocci.go
@@ -1,31 +1,31 @@
 package algorithms
 
 import (
-    "log"
+  "log"
 )
 
 func fibonocci(n int64) int64 {
-    if n == 0 || n == 1 {
-        return 1
-    }
-    memoize := make([]int64, n)
+  if n == 0 || n == 1 {
+    return 1
+  }
+  memoize := make([]int64, n)
 
-    for i := 0; int64(i) < n; i++ {
-        memoize[i] = 0
-    }
-    memoize[0] = 1
-    memoize[1] = 1
-    log.Printf("Memoize length: %v", len(memoize))
-    //fmt.Printf("Memoize: %v", memoize)
-    return helper(n-1, memoize)
+  for i := 0; int64(i) < n; i++ {
+    memoize[i] = 0
+  }
+  memoize[0] = 1
+  memoize[1] = 1
+  log.Printf("Memoize length: %v", len(memoize))
+  //fmt.Printf("Memoize: %v", memoize)
+  return helper(n-1, memoize)
 }
 
 func helper(n int64, memoize []int64) int64 {
-    i := memoize[n]
+  i := memoize[n]
 
-    if i != 0 {
-        return i
-    }
-    memoize[n] = helper(n-1, memoize) + helper(n-2, memoize)
-    return memoize[n]
+  if i != 0 {
+    return i
+  }
+  memoize[n] = helper(n-1, memoize) + helper(n-2, memoize)
+  return memoize[n]
 }
