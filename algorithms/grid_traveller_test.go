94a8873ef7aaef7d5a4987b8700bde555e2d75e4
Senthil Rajendran <rajsenthil@yahoo.com>
2022-04-14 19:12:00 -0700

    added grid test case

--- algorithms/grid_traveller_test.go
+++ algorithms/grid_traveller_test.go
@@ -12,6 +12,12 @@ func TestGridTravel(t *testing.T) {
 	gridTravel3_3 := gridTravel(3, 3)
 	log.Printf("Grid travel on 3x3 = %v", gridTravel3_3)
 
+	gridTravel6_6 := gridTravel(6, 6)
+	log.Printf("Grid travel on 6x6 = %v", gridTravel6_6)
+
+	gridTravel7_3 := gridTravel(7, 3)
+	log.Printf("Grid travel on 7x3 = %v", gridTravel7_3)
+
 	gridTravel18_18 := gridTravel(18, 18)
 	log.Printf("Grid travel on 18x18 = %v", gridTravel18_18)
 }
