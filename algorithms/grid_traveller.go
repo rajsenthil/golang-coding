ef3f78877d4d468a7b91193a151ab87654360a30
Senthil Rajendran <rajsenthil@yahoo.com>
2022-04-14 19:20:14 -0700

    whitespcae fixes

--- algorithms/grid_traveller.go
+++ algorithms/grid_traveller.go
@@ -1,33 +1,33 @@
 package algorithms
 
 func gridTravel(m int, n int) int64 {
-	memoize2 := make([][]int64, m+1)
-	for i := 0; i < m+1; i++ {
-		memoize2[i] = make([]int64, n+1)
-		for j := 0; j < n+1; j++ {
-			if m == 1 && n == 1 {
-				memoize2[i][j] = 1
-			} else if m == 0 || n == 0 {
-				memoize2[i][j] = 0
-			} else {
-				memoize2[i][j] = -1
-			}
-		}
-	}
-	return helper2(m, n, memoize2)
+    memoize2 := make([][]int64, m+1)
+    for i := 0; i < m+1; i++ {
+        memoize2[i] = make([]int64, n+1)
+        for j := 0; j < n+1; j++ {
+            if m == 1 && n == 1 {
+                memoize2[i][j] = 1
+            } else if m == 0 || n == 0 {
+                memoize2[i][j] = 0
+            } else {
+                memoize2[i][j] = -1
+            }
+        }
+    }
+    return helper2(m, n, memoize2)
 }
 
 func helper2(m int, n int, memoize2 [][]int64) int64 {
-	if m == 1 && n == 1 {
-		return 1
-	}
-	if m == 0 || n == 0 {
-		return 0
-	}
+    if m == 1 && n == 1 {
+        return 1
+    }
+    if m == 0 || n == 0 {
+        return 0
+    }
 
-	i := memoize2[m][n]
-	if i == -1 {
-		memoize2[m][n] = helper2(m-1, n, memoize2) + helper2(m, n-1, memoize2)
-	}
-	return memoize2[m][n]
+    i := memoize2[m][n]
+    if i == -1 {
+        memoize2[m][n] = helper2(m-1, n, memoize2) + helper2(m, n-1, memoize2)
+    }
+    return memoize2[m][n]
 }
--- algorithms/grid_traveller_test.go
+++ algorithms/grid_traveller_test.go
@@ -1,23 +1,23 @@
 package algorithms
 
 import (
-	"log"
-	"testing"
+    "log"
+    "testing"
 )
 
 func TestGridTravel(t *testing.T) {
-	gridTravel2_3 := gridTravel(2, 3)
-	log.Printf("Grid travel on 2x3 = %v", gridTravel2_3)
+    gridTravel2_3 := gridTravel(2, 3)
+    log.Printf("Grid travel on 2x3 = %v", gridTravel2_3)
 
-	gridTravel3_3 := gridTravel(3, 3)
-	log.Printf("Grid travel on 3x3 = %v", gridTravel3_3)
+    gridTravel3_3 := gridTravel(3, 3)
+    log.Printf("Grid travel on 3x3 = %v", gridTravel3_3)
 
-	gridTravel6_6 := gridTravel(6, 6)
-	log.Printf("Grid travel on 6x6 = %v", gridTravel6_6)
+    gridTravel6_6 := gridTravel(6, 6)
+    log.Printf("Grid travel on 6x6 = %v", gridTravel6_6)
 
-	gridTravel7_3 := gridTravel(7, 3)
-	log.Printf("Grid travel on 7x3 = %v", gridTravel7_3)
+    gridTravel7_3 := gridTravel(7, 3)
+    log.Printf("Grid travel on 7x3 = %v", gridTravel7_3)
 
-	gridTravel18_18 := gridTravel(18, 18)
-	log.Printf("Grid travel on 18x18 = %v", gridTravel18_18)
+    gridTravel18_18 := gridTravel(18, 18)
+    log.Printf("Grid travel on 18x18 = %v", gridTravel18_18)
 }
